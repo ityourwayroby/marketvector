2b709cd53400ab2d98de76bbaa71aef4052c10a1
ityourwayroby <152334001+ityourwayroby@users.noreply.github.com>
2024-07-30 06:08:02 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "marketvector-s3-bucket"
+    bucket         = "marketvector-s3-bucket-1"
     key            = "terraform_statefile"   
     region         = "us-east-1"
-    dynamodb_table = "marketvector-dynamodb" 
+    dynamodb_table = "new-dynamodb" 
   }
 }
