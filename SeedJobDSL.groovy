66c62d0ee4183240601ef734668db5e93fdc9b7e
ityourwayroby <152334001+ityourwayroby@users.noreply.github.com>
2024-08-09 09:41:00 +0100

    Update SeedJobDSL.groovy

--- SeedJobDSL.groovy
+++ SeedJobDSL.groovy
@@ -3,28 +3,33 @@ import groovy.json.JsonSlurper
 def pipelinesListFileName = 'pipelines_list.json'
 
 // Read the pipelines list from JSON file
-def projects = new JsonSlurper().parseText(new File("${WORKSPACE}", pipelinesListFileName).text)
+def config = new JsonSlurper().parseText(new File("${WORKSPACE}", pipelinesListFileName).text)
 
-// Loop through the projects and create pipelines
-projects.each { project ->
-    def pipelineName = project.name
-    def repoUrl = project.repo
-    def branch = project.branch
-    def jenkinsfilePath = project.jenkinsfile
-    def credentialsId = 'github_creds'
+// Loop through the folders
+config['rbac-folders'].each { folderName ->
+    folder(folderName) // Create a folder for each RBAC group
 
-    // Create pipeline job
-    pipelineJob(pipelineName) {
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        remote {
-                            url(repoUrl)
-                            credentials(credentialsId)
+    // Loop through the projects in each folder and create pipelines
+    config[folderName].each { project ->
+        def pipelineName = project.name
+        def repoUrl = project.repo
+        def branch = project.branch
+        def jenkinsfilePath = project.jenkinsfile
+        def credentialsId = 'github_creds'
+
+        // Create pipeline job inside the folder
+        pipelineJob("${folderName}/${pipelineName}") {
+            definition {
+                cpsScm {
+                    scm {
+                        git {
+                            remote {
+                                url(repoUrl)
+                                credentials(credentialsId)
+                            }
+                            branches(branch)
+                            scriptPath(jenkinsfilePath)
                         }
-                        branches(branch)
-                        scriptPath(jenkinsfilePath)
                     }
                 }
             }
